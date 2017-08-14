ed6aa65c5b752327a1d75a35b6b87bf38593f980
Bobby <techguru1103@gmail.com>
2017-08-14 16:44:44 -0700

    First Commit

---
+++ PrometheusNew/Modules/Test.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrometheusNew.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("Test")]
+        public async Task Dickbag(string repeat)
+        {
+            await Context.Channel.SendMessageAsync(ToString());
+        }
+        
+    }
+}
---
+++ PrometheusNew/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace PrometheusNew
+{
+    public class Program
+    {
+        static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+        
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            
+            _client = new DiscordSocketClient();
+            
+            await _client.LoginAsync(TokenType.Bot, "MzQ2NDIxODAzMzM1Mjg2Nzg0.DHJ_kw.f2qMJ2E6wvzfTHYim_VCVmzXpws");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            await Task.Delay(-1);
+        }
+    }
+}
